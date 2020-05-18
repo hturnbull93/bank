edac3bd38bd6f6fa0c872d42312b63d86b6808b2
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 14:11:01 +0100

    G: debit makes new transaction with credit and resulting balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,16 @@
+require_relative './transaction.rb'
+
 class Account
   attr_reader :balance
 
-  def initialize
+  def initialize(transactionClass = Transaction)
     @balance = 0
+    @transactionClass = transactionClass
   end
 
   def deposit(amount)
     @balance += amount
+    transaction = @transactionClass.new(credit: amount, balance: @balance)
   end
 
   def withdraw(amount)
@@ -17,8 +21,8 @@ class Account
 
   def statement
     "date || credit || debit || balance
-14/01/2012 || || 500.00 || 2500.00
-13/01/2012 || 2000.00 || || 3000.00
-10/01/2012 || 1000.00 || || 1000.00"
+14/01/2012 || || 500.00 || 2500.00 
+13/01/2012 || 2000.00 || || 3000.00 
+10/01/2012 || 1000.00 || || 1000.00 "
   end
 end
