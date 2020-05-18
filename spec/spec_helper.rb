40b84b7ebe6a78682d7849c6a7d99d96eaa66a5b
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 15:19:10 +0100

    linted

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require 'simplecov'
 require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(SimpleCov::Formatter::Console)
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  SimpleCov::Formatter::Console
+)
 SimpleCov.start
 
 RSpec.configure do |config|
