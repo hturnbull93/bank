2cf3bf4e3aa3a0bf46a506d44d32beef96b387c0
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 12:56:23 +0100

    reworked test to assume transaction uses current time

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,8 @@
+require 'transaction'
+
 describe Transaction do
-  it 'can be initialised with a Time' do
-    testTime = Time.new(2020,05,18)
-    subject = Transaction.new(time: testTime)
-    expect(subject.display).to eq '18/05/2020 || || || '
+  it '.display' do
+    testTimeFormatted = Time.now.strftime('%d/%m/%Y"')
+    expect(subject.display).to eq "#{testTimeFormatted} || || || "
   end
 end
