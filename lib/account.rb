cbc1d7072330304a67aed8b83301910e848bfd39
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 14:35:14 +0100

    G: transactionistory added to by transactions, statement loops through array

--- lib/account.rb
+++ lib/account.rb
@@ -6,24 +6,27 @@ class Account
   def initialize(transactionClass = Transaction)
     @balance = 0
     @transactionClass = transactionClass
+    @transactionHistory = []
   end
 
   def deposit(amount)
     @balance += amount
-    transaction = @transactionClass.new(credit: amount, balance: @balance)
+    @transactionHistory.unshift @transactionClass.new(credit: amount, balance: @balance)
   end
   
   def withdraw(amount)
     raise 'Insufficient funds' if @balance < amount
     
     @balance -= amount
-    transaction = @transactionClass.new(debit: amount, balance: @balance)
+    @transactionHistory.unshift @transactionClass.new(debit: amount, balance: @balance)
   end
 
   def statement
-    "date || credit || debit || balance
-14/01/2012 || || 500.00 || 2500.00 
-13/01/2012 || 2000.00 || || 3000.00 
-10/01/2012 || 1000.00 || || 1000.00 "
+    header = "date || credit || debit || balance\n"
+    statementRows = @transactionHistory.map do |transaction|
+      transaction.display
+    end
+
+    header + statementRows.join("\n")
   end
 end
