b464df004768e6df079693eca2bdf4f32b1bea3a
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 13:14:44 +0100

    G: .display shows credit it was initialsied with

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,13 +1,20 @@
 class Transaction
-  def initialize
+  def initialize(credit: nil)
     @time = Time.now
+    @credit = credit
   end
 
   def display
-    "#{formatTime} || || || "
+    "#{formatTime} || #{creditFormat}|| || "
   end
 
   private
+
+  def creditFormat
+    if @credit
+      "#{@credit} "
+    end
+  end
 
   def formatTime
     @time.strftime('%d/%m/%Y')
