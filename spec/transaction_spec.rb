4999d51ac6153ca8f53d82038805c1d08de57a72
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 13:08:03 +0100

    R: transaction.siplay shows current time formatted

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,8 +1,13 @@
 require 'transaction'
 
 describe Transaction do
-  it '.display' do
-    testTimeFormatted = Time.now.strftime('%d/%m/%Y')
-    expect(subject.display).to eq "#{testTimeFormatted} || || || "
+  describe '.display' do
+    before(:each) do
+      @testTimeFormatted = Time.now.strftime('%d/%m/%Y')
+    end
+
+    it 'has the time in the first column' do
+      expect(subject.display).to eq "#{@testTimeFormatted} || || || "
+    end
   end
 end
