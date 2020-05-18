2ed4035eae0d072ce053bdb2723bd5f2ae8bd9b9
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 20:49:37 +0100

    adjust tests to use pence figures

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,22 +9,22 @@ describe Transaction do
     end
     
     it 'has the credit it is initialised with in the second column' do
-      subject = described_class.new(credit: 500)
+      subject = described_class.new(credit: 50000)
       expect(subject.display).to eq "#{test_time_format} || 500.00 || || "
     end
     
     it 'has the debit it is initialised with in the second column' do
-      subject = described_class.new(debit: 500)
+      subject = described_class.new(debit: 50000)
       expect(subject.display).to eq "#{test_time_format} || || 500.00 || "
     end
 
     it 'has the balance it is initialised with in the third column' do
-      subject = described_class.new(balance: 2000)
+      subject = described_class.new(balance: 200000)
       expect(subject.display).to eq "#{test_time_format} || || || 2000.00 "
     end
     
     it 'displays time, credit, debit, balance all at once' do
-      subject = described_class.new(credit: 200, debit:100, balance: 2000)
+      subject = described_class.new(credit: 20000, debit:10000, balance: 200000)
 
       expect(subject.display).to eq "#{test_time_format} || 200.00 || 100.00 || 2000.00 "
     end
