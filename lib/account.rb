d38973acd7b97f4d867a5393f69d25d5f0f70f04
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 14:43:44 +0100

    RF: addWithdrawTransaction and addDepositTransaction

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative './transaction.rb'
 
 class Account
@@ -11,22 +13,28 @@ class Account
 
   def deposit(amount)
     @balance += amount
-    @transactionHistory.unshift @transactionClass.new(credit: amount, balance: @balance)
+    addDepositTransaction(credit: amount, balance: @balance)
   end
-  
+
   def withdraw(amount)
     raise 'Insufficient funds' if @balance < amount
-    
+
     @balance -= amount
-    @transactionHistory.unshift @transactionClass.new(debit: amount, balance: @balance)
+    addWithdrawTransaction(debit: amount, balance: @balance)
   end
 
   def statement
     header = "date || credit || debit || balance\n"
-    statementRows = @transactionHistory.map do |transaction|
-      transaction.display
-    end
+    statementRows = @transactionHistory.map(&:display)
 
     header + statementRows.join("\n")
+  end
+
+  def addDepositTransaction(credit: nil, balance: nil)
+    @transactionHistory.unshift @transactionClass.new(credit: credit, balance: balance)
+  end
+
+  def addWithdrawTransaction(debit: nil, balance: nil)
+    @transactionHistory.unshift @transactionClass.new(debit: debit, balance: balance)
   end
 end
