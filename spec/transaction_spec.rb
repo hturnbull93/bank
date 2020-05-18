4cf97df50ac1dbbe3c742ff43f97cf9c23f647fe
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 13:29:13 +0100

    R: General test, alsoadjusted for .00 after figures

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -12,17 +12,22 @@ describe Transaction do
 
     it 'has the credit it is initialised with in the second column' do
       subject = Transaction.new(credit: 500)
-      expect(subject.display).to eq "#{@testTimeFormatted} || 500 || || "
+      expect(subject.display).to eq "#{@testTimeFormatted} || 500.00 || || "
     end
 
     it 'has the debit it is initialised with in the second column' do
       subject = Transaction.new(debit: 500)
-      expect(subject.display).to eq "#{@testTimeFormatted} || || 500 || "
+      expect(subject.display).to eq "#{@testTimeFormatted} || || 500.00 || "
     end
 
     it 'has the balance it is initialised with in the third column' do
       subject = Transaction.new(balance: 2000)
-      expect(subject.display).to eq "#{@testTimeFormatted} || || || 2000"
+      expect(subject.display).to eq "#{@testTimeFormatted} || || || 2000.00 "
+    end
+
+    it 'displays time, credit, debit, balance all at once' do
+      subject = Transaction.new(credit: 200, debit: 100, balance: 2000)
+      expect(subject.display).to eq "#{@testTimeFormatted} || 200.00 || 100.00 || 2000.00 "
     end
   end
 end
