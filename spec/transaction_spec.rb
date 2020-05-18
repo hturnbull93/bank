b76f02045c697b2ca78f8efff9370861eb5261b2
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 15:19:22 +0100

    use snake_case for variables

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,31 +5,31 @@ require 'transaction'
 describe Transaction do
   describe '.display' do
     before(:each) do
-      @testTimeFormatted = Time.now.strftime('%d/%m/%Y')
+      @test_time_format = Time.now.strftime('%d/%m/%Y')
     end
 
     it 'has the time in the first column' do
-      expect(subject.display).to eq "#{@testTimeFormatted} || || || "
+      expect(subject.display).to eq "#{@test_time_format} || || || "
     end
 
     it 'has the credit it is initialised with in the second column' do
       subject = Transaction.new(credit: 500)
-      expect(subject.display).to eq "#{@testTimeFormatted} || 500.00 || || "
+      expect(subject.display).to eq "#{@test_time_format} || 500.00 || || "
     end
 
     it 'has the debit it is initialised with in the second column' do
       subject = Transaction.new(debit: 500)
-      expect(subject.display).to eq "#{@testTimeFormatted} || || 500.00 || "
+      expect(subject.display).to eq "#{@test_time_format} || || 500.00 || "
     end
 
     it 'has the balance it is initialised with in the third column' do
       subject = Transaction.new(balance: 2000)
-      expect(subject.display).to eq "#{@testTimeFormatted} || || || 2000.00 "
+      expect(subject.display).to eq "#{@test_time_format} || || || 2000.00 "
     end
 
     it 'displays time, credit, debit, balance all at once' do
       subject = Transaction.new(credit: 200, debit: 100, balance: 2000)
-      expect(subject.display).to eq "#{@testTimeFormatted} || 200.00 || 100.00 || 2000.00 "
+      expect(subject.display).to eq "#{@test_time_format} || 200.00 || 100.00 || 2000.00 "
     end
   end
 end
