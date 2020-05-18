ea45a0af51effc75ffddcd232ec8a2158ac162c9
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 18:41:09 +0100

    R: Hi There prints Hi There with newline

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -5,6 +5,8 @@ describe Printer do
     it '"Hello World" prints "Hello World" with newline' do
       expect { subject.print('Hello World') }.to output("Hello World\n").to_stdout
     end
-      
+    it '"Hi There" prints "Hi There" with newline' do
+      expect { subject.print('Hi There') }.to output("Hi There\n").to_stdout
+    end
   end
 end
