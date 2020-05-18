89643dde4058b8f8a63b9be94dd4c85c4993272d
Harry Turnbull <hturnbull@live.co.uk>
2020-05-18 18:27:26 +0100

    extracted helper method for test_time_format

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,37 +5,32 @@ require 'transaction'
 describe Transaction do
   describe '.display' do
     it 'has the time in the first column' do
-      @test_time_format = Time.now.strftime('%d/%m/%Y')
-
-      expect(subject.display).to eq "#{@test_time_format} || || || "
+      expect(subject.display).to eq "#{test_time_format} || || || "
     end
     
     it 'has the credit it is initialised with in the second column' do
-      @test_time_format = Time.now.strftime('%d/%m/%Y')
       subject = described_class.new(credit: 500)
-
-      expect(subject.display).to eq "#{@test_time_format} || 500.00 || || "
+      expect(subject.display).to eq "#{test_time_format} || 500.00 || || "
     end
     
     it 'has the debit it is initialised with in the second column' do
-      @test_time_format = Time.now.strftime('%d/%m/%Y')
       subject = described_class.new(debit: 500)
-
-      expect(subject.display).to eq "#{@test_time_format} || || 500.00 || "
+      expect(subject.display).to eq "#{test_time_format} || || 500.00 || "
     end
 
     it 'has the balance it is initialised with in the third column' do
-      @test_time_format = Time.now.strftime('%d/%m/%Y')
       subject = described_class.new(balance: 2000)
-
-      expect(subject.display).to eq "#{@test_time_format} || || || 2000.00 "
+      expect(subject.display).to eq "#{test_time_format} || || || 2000.00 "
     end
     
     it 'displays time, credit, debit, balance all at once' do
-      @test_time_format = Time.now.strftime('%d/%m/%Y')
-      subject = described_class.new(credit: 200, debit: 100, balance: 2000)
+      subject = described_class.new(credit: 200, debit:100, balance: 2000)
 
-      expect(subject.display).to eq "#{@test_time_format} || 200.00 || 100.00 || 2000.00 "
+      expect(subject.display).to eq "#{test_time_format} || 200.00 || 100.00 || 2000.00 "
     end
   end
+end
+
+def test_time_format
+  Time.now.strftime('%d/%m/%Y')
 end
